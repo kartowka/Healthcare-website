cf675af57443b8f5a44a6d1e73e3be88d62697cb
Sharon Yaroshetsky <imso3k@gmail.com>
2021-03-05 15:43:07 +0200

    updated linter with more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,56 @@
 module.exports =
-    {
-        'env':           {
-            'browser':  true,
-            'commonjs': true,
-            'es6':      true,
-            'node':     true,
-        },
-        'extends':       'eslint:recommended',
-        'globals':       {
-            'Atomics':           'readonly',
-            'SharedArrayBuffer': 'readonly',
-        },
-        'parserOptions': {
-            'ecmaVersion': 2018,
-        },
-        'rules':         {
-            'quotes': [
+{
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true,
+        'node': true,
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly',
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+    },
+    'rules': {
+        'quotes': [
+            'error',
+            'single',
+        ],
+
+        'semi': [
+            'error',
+            'never',
+        ],
+        'for-direction'
+            : [
                 'error',
-                'single',
             ],
-            'semi':   [
-                'error',
-                'never',
-            ],
-        },
-    }
+
+        'no-dupe-else-if': [
+            'error',
+        ],
+
+        'no-constant-condition': [
+            'error',
+        ],
+
+        'no-empty': [ //no empty blocks
+            'error',
+        ],
+
+        'block-scoped-var': [ //use var only in scope
+            'error',
+        ],
+
+        'default-case': [
+            'error',
+        ],
+
+        'default-param-last': [
+            'error',
+        ],
+    },
+}
