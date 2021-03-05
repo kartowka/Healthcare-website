3e076414a023b0ee4503ff0cc829daeb53f1fef8
kartowka <antonfley@gmail.com>
2021-03-05 17:26:49 +0200

    fix indent

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ app.set('view engine', 'ejs')
 //Static File 
 
 app.use(express.static(path.join(__dirname, '/views/')))
-app.set('views',__dirname+'/views/')
-app.use('/css',express.static(__dirname +'/css'))
-app.use('/scripts',express.static(__dirname +'/scripts'))
+app.set('views', __dirname + '/views/')
+app.use('/css', express.static(__dirname + '/css'))
+app.use('/scripts', express.static(__dirname + '/scripts'))
 //app.use('/img',express.static(__dirname +'public/img'))
 
 
@@ -20,11 +20,11 @@ app.use('/scripts',express.static(__dirname +'/scripts'))
 //    res.sendFile(__dirname + '/login.html')
 //})
 
-app.get('/', (req, res)=> {
-    res.render('index',{title: 'Home Page'})
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page' })
 })
- 
-app.listen(appPort,() => {
+
+app.listen(appPort, () => {
     console.log(`App is running. serve at port: ${appPort}`)
     console.log(`http://127.0.0.1:${appPort}`)
 })
