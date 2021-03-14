42d008f8e74668c3ba7271afcffbdbc1f5c6de22
kartowka <antonfley@gmail.com>
2021-03-14 16:11:52 +0200

    updated eslint errors for custom.js file

--- src/js/custom.js
+++ src/js/custom.js
@@ -5,12 +5,12 @@ jQuery(document).ready(function () {
 
     //this code is for smooth scroll and nav selector
     $(document).ready(function () {
-        $(document).on("scroll", onScroll);
+        $(document).on('scroll', onScroll);
 
         //smoothscroll
         $('a[href^="#"]').on('click', function (e) {
             e.preventDefault();
-            $(document).off("scroll");
+            $(document).off('scroll');
 
             $('a').each(function () {
                 $(this).removeClass('active');
@@ -24,7 +24,7 @@ jQuery(document).ready(function () {
                 'scrollTop': $target.offset().top + 2
             }, 500, 'swing', function () {
                 window.location.hash = target;
-                $(document).on("scroll", onScroll);
+                $(document).on('scroll', onScroll);
             });
         });
     });
@@ -33,12 +33,12 @@ jQuery(document).ready(function () {
         var scrollPos = $(document).scrollTop();
         $('.navbar-default .navbar-nav>li>a').each(function () {
             var currLink = $(this);
-            var refElement = $(currLink.attr("href"));
+            var refElement = $(currLink.attr('href'));
             if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-                $('.navbar-default .navbar-nav>li>a').removeClass("active");
-                currLink.addClass("active");
+                $('.navbar-default .navbar-nav>li>a').removeClass('active');
+                currLink.addClass('active');
             } else {
-                currLink.removeClass("active");
+                currLink.removeClass('active');
             }
         });
     }
@@ -49,27 +49,27 @@ jQuery(document).ready(function () {
         var windowScrollPosTop = jQuery(window).scrollTop();
 
         if (windowScrollPosTop >= 150) {
-            jQuery(".header").css({
-                "background": "#B193DD",
+            jQuery('.header').css({
+                'background': '#B193DD',
             });
-            jQuery(".top-header img.logo").css({
-                "margin-top": "-40px",
-                "margin-bottom": "0"
+            jQuery('.top-header img.logo').css({
+                'margin-top': '-40px',
+                'margin-bottom': '0'
             });
-            jQuery(".navbar-default").css({
-                "margin-top": "-15px",
+            jQuery('.navbar-default').css({
+                'margin-top': '-15px',
             });
         } else {
-            jQuery(".header").css({
-                "background": "transparent",
+            jQuery('.header').css({
+                'background': 'transparent',
             });
-            jQuery(".top-header img.logo").css({
-                "margin-top": "-15px",
-                "margin-bottom": "25px"
+            jQuery('.top-header img.logo').css({
+                'margin-top': '-15px',
+                'margin-bottom': '25px'
             });
-            jQuery(".navbar-default").css({
-                "margin-top": "12px",
-                "margin-bottom": "0"
+            jQuery('.navbar-default').css({
+                'margin-top': '12px',
+                'margin-bottom': '0'
             });
 
         }
