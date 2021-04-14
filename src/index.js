e6766a660d68da2e470f4d32d3fcde687d60fdd7
Sharon Yaroshetsky <imso3k@gmail.com>
2021-04-14 14:52:23 +0300

    added requires for new packages & jwt verification

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const path = require('path')
+const User = require('./models/userModel')
+const routes = require('./routes/route.js')
+
 const appPort = process.env.PORT || 4000
 const app = express()
-var path = require('path')
-const mongoose = require('mongoose')
-const User = require('./models/user')
+
+//.env
+require('dotenv').config({
+  path: path.join(__dirname, '../.env')
+})
 
 //connect to mongodb
-const dbURI = 'mongodb+srv://jsas:q8L74rSEkuhuWU6k@healthcare.eq86f.mongodb.net/healthcare-db?retryWrites=true&w=majority'
+const dbURI = 'mongodb+srv://jsas:q8L74rSEkuhuaU6k@healthcare.eq86f.mongodb.net/healthcare-db?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err))
 
+app.use(express.urlencoded({ extended: true }))
+
+app.use(async (req, res, next) => {
+  if (req.headers['x-access-token']) {
+    const accessToken = req.headers['x-access-token']
+    const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET)
+    // Check if token has expired
+    if (exp < Date.now().valueOf() / 1000) {
+      return res.status(401).json({ error: 'JWT token has expired, please login to obtain a new one' })
+    }
+    res.locals.loggedInUser = await User.findById(userId); next()
+  } else {
+    next()
+  }
+})
 
 //Setting up Handlebars
 app.set('view engine', 'ejs')
@@ -41,7 +65,6 @@ app.use(function (req, res, next) {
 
 })
 
-//mongoose and mongo sandbox routes
 
 //Creating a connection
 app.listen(appPort, () => {
