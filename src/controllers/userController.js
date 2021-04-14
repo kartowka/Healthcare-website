a681e759fe74c8e0d26c64601e16050ef3b7c221
kartowka <antonfley@gmail.com>
2021-04-14 12:46:05 +0300

    added eslint js folders check controlles models

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,27 +1,27 @@
 // server/controllers/userController.js
 
-const User = require('../models/userModel');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 async function hashPassword(password) {
- return await bcrypt.hash(password, 10);
+ return await bcrypt.hash(password, 10)
 }
 
 async function validatePassword(plainPassword, hashedPassword) {
- return await bcrypt.compare(plainPassword, hashedPassword);
+ return await bcrypt.compare(plainPassword, hashedPassword)
 }
 
 exports.signup = async (req, res, next) => {
  try {
   const { email, password, role } = req.body
-  const hashedPassword = await hashPassword(password);
-  const newUser = new User({ email, password: hashedPassword, role: role || 'basic' });
+  const hashedPassword = await hashPassword(password)
+  const newUser = new User({ email, password: hashedPassword, role: role || 'basic' })
   const accessToken = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
    expiresIn: '1d'
-  });
-  newUser.accessToken = accessToken;
-  await newUser.save();
+  })
+  newUser.accessToken = accessToken
+  await newUser.save()
   res.json({
    data: newUser,
    accessToken
