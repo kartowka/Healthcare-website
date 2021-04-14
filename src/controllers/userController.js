84be1ce78c46247880a21c3520b3c3d895dd92e4
kartowka <antonfley@gmail.com>
2021-04-14 12:35:03 +0300

    created connection to mongoDB

---
+++ src/controllers/userController.js
@@ -0,0 +1,32 @@
+// server/controllers/userController.js
+
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+async function hashPassword(password) {
+ return await bcrypt.hash(password, 10);
+}
+
+async function validatePassword(plainPassword, hashedPassword) {
+ return await bcrypt.compare(plainPassword, hashedPassword);
+}
+
+exports.signup = async (req, res, next) => {
+ try {
+  const { email, password, role } = req.body
+  const hashedPassword = await hashPassword(password);
+  const newUser = new User({ email, password: hashedPassword, role: role || "basic" });
+  const accessToken = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+   expiresIn: "1d"
+  });
+  newUser.accessToken = accessToken;
+  await newUser.save();
+  res.json({
+   data: newUser,
+   accessToken
+  })
+ } catch (error) {
+  next(error)
+ }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const appPort = process.env.PORT || 4000
 const app = express()
 var path = require('path')
+const mongoose = require('mongoose');
+const User = require('./models/user');
+
+//connect to mongodb
+const dbURI = "mongodb+srv://jsas:q8L74rSEkuhuWU6k@healthcare.eq86f.mongodb.net/healthcare-db?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 
 //Setting up Handlebars
 app.set('view engine', 'ejs')
@@ -10,7 +19,7 @@ app.set('views', __dirname + '/views/')
 app.use('/css', express.static(__dirname + '/css'))
 app.use('/fonts', express.static(__dirname + '/fonts'))
 app.use('/js', express.static(__dirname + '/js'))
-app.use('/img',express.static(__dirname +'/img'))
+app.use('/img', express.static(__dirname + '/img'))
 
 
 //routing
@@ -18,20 +27,23 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Home Page' })
 })
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
   res.render('login', { title: 'login' })
 })
 
 
-app.use( function(req, res, next) {
+app.use(function (req, res, next) {
 
-    // you can do what ever you want here
-    // for example rendering a page with '404 Not Found'
-    res.status(404)
-    res.render('404', { error: 'Not Found'})
+  // you can do what ever you want here
+  // for example rendering a page with '404 Not Found'
+  res.status(404)
+  res.render('404', { error: 'Not Found' })
 
 })
 
+//mongoose and mongo sandbox routes
+
+app.get('/add-')
 //Creating a connection
 app.listen(appPort, () => {
   console.log(`App is running. serve at port: ${appPort}`)
