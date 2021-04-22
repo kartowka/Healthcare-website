efd701b61fb63b25c8dc35c1a0f576b0441ae18e
Sharon Yaroshetsky <imso3k@gmail.com>
2021-04-22 15:13:58 +0300

    re-factored validity, added password strength check

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // set initially button state hidden
-    $('#btn').hide()
+    $('#btn').attr('disabled', true)
     // use keyup event on email field
     $('#email').keyup(function () {
         if (validateEmail()) {
@@ -16,56 +16,85 @@ $(document).ready(function () {
         buttonState()
     })
     // use keyup event on password
-    $('#pwd').keyup(function () {
+    $('#pwd_main').keyup(function () {
         // check
-        if (validatePassword()) {
+    
+        buttonState()
+    })
+
+    $('#pwd_rep').keyup(function () {
+        // check
+        if (validatePasswordPairLength()) {
             // set input password border green
-            $('#pwd').css('border', '2px solid green')
+            $('#pwd_main').css('border', '2px solid green')
+            $('#pwd_rep').css('border', '2px solid green')
             //set passMsg 
         } else {
             // set input password border red
-            $('#pwd').css('border', '2px solid red')
+            $('#pwd_main').css('border', '2px solid red')
+            $('#pwd_rep').css('border', '2px solid red')
             //set passMsg 
         }
         buttonState()
     })
-  })
-  
-  function buttonState() {
-    if (validateEmail() && validatePassword()) {
+})
+
+function buttonState() {
+    if (validateEmail() && checkPassStrength()) {
         // if the both email and password are validate
         // then button should show visible
-        $('#btn').show()
+        $('#btn').attr('disabled', false)
     } else {
         // if both email and pasword are not validated
         // button state should hidden
-        $('#btn').hide()
+        $('#btn').attr('disabled', true)
     }
-  }
-  function validateEmail() {
+}
+function validateEmail() {
     // get value of input email
     var email = $('#email').val()
     // use reular expression
-    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+    var reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
     if (reg.test(email)) {
         return true
     } else {
         return false
     }
-  
-  }
-  function validatePassword() {
-    //get input password value
-    var pwd = $('#pwd').val()
-    // check it s length
-    if (pwd.length > 7) {
-        return true
+
+}
+function checkPassStrength() {
+    var number = /([0-9])/
+    var alphabets = /([a-zA-Z])/
+    var special_characters = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/
+    var pwd = $('#pwd_main').val()
+    if (pwd.length < 8) {
+        $('#pwd_main-strength-status').removeClass()
+        $('#pwd_main-strength-status').addClass('weak-pass')
+        $('#pwd_main-strength-status').html('<b>Weak</b> (At least 8 characters in length)')
     } else {
-        return false
+        if (pwd.match(number) && pwd.match(alphabets) && pwd.match(special_characters) && pwd.match(/[A-Z]/)) {
+            $('#pwd_main-strength-status').removeClass()
+            $('#pwd_main-strength-status').addClass('strong-pass')
+            $('#pwd_main-strength-status').html('<b>Very Strong</b>')
+        } else {
+            $('#pwd_main-strength-status').removeClass()
+            $('#pwd_main-strength-status').addClass('medium-pass')
+            $('#pwd_main-strength-status').html('<b>Medium</b> (Should include Alphabets, Numbers, Special characters, and 1 Capital letter)')
+        }
     }
-  
-  }
-  function showDiv(divId, element)
-{
+}
+
+function validatePasswordPairLength() {
+    //get input password value
+    var pwd_main = $('#pwd_main').val()
+    var pwd_rep = $('#pwd_rep').val()
+
+    if (pwd_main != pwd_rep) {
+        return false
+    } else {
+        return true
+    }
+}
+function showDiv(divId, element) {
     document.getElementById(divId).style.display = element.value == 'doctor' ? 'block' : 'none'
 }
