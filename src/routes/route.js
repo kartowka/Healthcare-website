3ecf0d5f35697f425686bf7fd7f018f8caa5ed5d
kartowka <antonfley@gmail.com>
2021-04-25 23:40:33 +0300

    rfix middleware router function, for proper routing

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,45 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
+
 //homepage
-router.get('/', function (req, res) {
-    res.render('index')
-})
+router
+    .route('/')
+    .get((req, res) => {
+        res.render('index')
+    })
+//login
+router
+    .route('/login')
+    .get((req, res) => {
+        res.render('login')
+    })
+    .post(userController.login)
+//logout
+router
+    .route('/logout')
+    .get((req, res) => {
+        res.clearCookie('x-access-token')
+        res.redirect('/')
+    })
+//signup
+router
+    .route('/register')
+    .get((req, res) => {
+        res.render('register')
+    })
+    .post(userController.signup)
+
+//forgot password
+router
+    .route('/forgot-password')
+    .get((req, res) => {
+        res.render('forgot-password')
+    })
+
+router
+    .route('/confirm/:confirmationCode')
+    .get(userController.verifyUser)
 
 //pateint profile
 router.get('/patientProfile', function (req, res) {
@@ -26,40 +61,16 @@ router.get('/patientProfile/previousAppointments', function (req, res) {
 router.get('/patientProfile/futureAppointments', function (req, res) {
     res.render('futureAppointments')
 })
+router
+.route('/user/:userId')
+.get(userController.allowIfLoggedin,userController.getUser)
+//.put('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'profile'), userController.updateUser)
+//.delete('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser)
 
+router
+.route('/users')
+.get(userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), userController.getUsers)
 
-//signup page
-router.get('/register', function (req, res) {
-    res.render('register')
-})
-
-//forgot password
-router.get('/forgot-password', function (req, res) {
-    res.render('forgot-password')
-})
-
-router.post('/register', userController.signup)
-
-//login page
-router.get('/login', function (req, res) {
-    res.render('login')
-})
-router.post('/login', userController.login)
-router.get('/logout',function(req, res) {
-    res.clearCookie('x-access-token')
-    res.redirect('/')
-}) 
-router.get('/confirm/:confirmationCode', function(req,res){
-    console.log(req.params.confiramationCode)
-} )
-
-router.get('/user/:userId', userController.allowIfLoggedin, userController.getUser)
-
-router.get('/users', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), userController.getUsers)
-
-router.put('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'profile'), userController.updateUser)
-
-router.delete('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser)
 
 router.use(function (req, res, next) {
 
@@ -67,7 +78,7 @@ router.use(function (req, res, next) {
     // for example rendering a page with '404 Not Found'
     res.status(404)
     res.render('404', { error: 'Not Found' })
-  
-  })
+
+})
 
 module.exports = router
