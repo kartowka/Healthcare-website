04e50a916cf4aaedd186823697647d2c5a30fd8f
Sharon Yaroshetsky <imso3k@gmail.com>
2021-04-27 13:24:26 +0300

    changed which mail is sent by user type, added a function for doctor auth confirmation by email

--- src/js/nodemailer.js
+++ src/js/nodemailer.js
@@ -15,18 +15,47 @@ const transport = nodemailer.createTransport({
     },
 })
 
-module.exports.sendConfirmationEmail = (first_name,last_name, email, confirmationCode) => {
-    transport.sendMail({
-        from: user,
-        to: email,
-        subject: 'Reset your account password',
-        html: `<h1>Email Confirmation</h1>
+module.exports.sendConfirmationEmail = (first_name,last_name, email, confirmationCode, role) => {
+    if (role == 'doctor'){
+        transport.sendMail({
+            from: user,
+            to: email,
+            subject: 'Activate Your Account',
+            html: `<h1>Email Confirmation</h1>
+        <h2>Hello Dr.${last_name}</h2>
+        <p>Thank you for registering to our HealthCare system. Please confirm your email by clicking on the following link</p>
+        <a href=http://127.0.0.1:4000/confirm/${confirmationCode}> Click here</a>
+        <p>Please wait for an Admin to authenticate your credentials afterwards</p>
+        </div>`,
+        }).catch(err => console.log(err))
+    } else {
+        transport.sendMail({
+            from: user,
+            to: email,
+            subject: 'Activate Your Account',
+            html: `<h1>Email Confirmation</h1>
         <h2>Hello ${first_name} ${last_name}</h2>
         <p>Thank you for registering to our HealthCare system. Please confirm your email by clicking on the following link</p>
         <a href=http://127.0.0.1:4000/confirm/${confirmationCode}> Click here</a>
         </div>`,
+        }).catch(err => console.log(err))
+    }
+}
+
+module.exports.sendAuthenticationApprovalToDoctor = (last_name, email, id) => {
+
+    transport.sendMail({
+        from: user,
+        to: email,
+        subject: 'You Have Been Authenticated',
+        html: `<h1>Email Confirmation</h1>
+        <h2>Hello Dr.${last_name}</h2>
+        <p>You have been authenticated by our admin, you can keep using our system with full doctor privileges</p>
+        <a href=http://127.0.0.1:4000/doctor_profile/:${id}> Click here</a>
+        </div>`,
     }).catch(err => console.log(err))
 }
+
 module.exports.sendResetPassword = (first_name,last_name, email, confirmationCode) => {
     transport.sendMail({
         from: user,
