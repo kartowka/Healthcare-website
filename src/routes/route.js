118ca3d785403804e704cddcda86b0e13cc2dfb3
Sharon Yaroshetsky <imso3k@gmail.com>
2021-04-28 21:52:12 +0300

    removed unused requires

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const user_controller = require('../controllers/user_controller')
 const profile_router = require('./profile_route')
-const User = require('../models/user_model')
 
 //homepage
 router
