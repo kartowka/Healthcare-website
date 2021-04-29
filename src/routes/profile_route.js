7d6e5f343b8d5dec8b5924618e17d6b971a7a585
kartowka <antonfley@gmail.com>
2021-04-29 10:56:52 +0300

    fixed redirect routing

--- src/routes/profile_route.js
+++ src/routes/profile_route.js
@@ -6,30 +6,40 @@ const user_controller = require('../controllers/user_controller')
 //pateint profile
 profile
     .route('/patient_profile/:id') 
-    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser)
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('patient_profile',{data:req.user})
+    })
 
 //settings patient profile
 
 profile
-    .route(['/settings_patient_profile/:id', '/patient_profile/settings_patient_profile/:id'])
-    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readOwn', 'patient_profile'), user_controller.getUser)
+    .route('/patient_profile/settings_patient_profile/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readOwn', 'patient_profile'), user_controller.getUser,(req,res)=>{
+        res.render('settings_patient_profile',{data:req.user})
+    })
     .post(user_controller.allowIfLoggedin,user_controller.grantAccess('updateOwn','patient_profile'),user_controller.updateUser)
 
 profile
-    .route([ '/previous_appointments/:id', '/patient_profile/previous_appointments/:id'])
-    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readOwn', 'patient_profile'), user_controller.getUser)
+    .route('/patient_profile/previous_appointments/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readOwn', 'patient_profile'), user_controller.getUser,(req,res)=>{
+        res.render('previous_appointments',{data:req.user})
+    })
     .post(user_controller.allowIfLoggedin, user_controller.grantAccess('updateOwn', 'patient_profile'), user_controller.updateUser)
 
 //previous and future appointments
 profile
-    .route(['/previous_appointments/:id', '/patient_profile/previous_appointments/:id','/future_appointments/:id', '/patient_profile/future_appointments/:id'])
-    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser)
+    .route('/patient_profile/future_appointments/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('future_appointments',{data:req.user})
+    })
     
 
 //doctor profile
 profile
     .route('/doctor_profile/:id')
-    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny','patient_profile'),user_controller.grantAccess('readAny','doctor_profile'), user_controller.getUser)
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny','patient_profile'),user_controller.grantAccess('readAny','doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('doctor_profile',{data:req.user})
+    })
     .post(user_controller.allowIfLoggedin,user_controller.grantAccess('updateOwn','doctor_profile'),user_controller.updateUser)
 // TBD
 
