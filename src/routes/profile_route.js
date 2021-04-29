3fa147f9052831159b3ccc07fcadce015c2dbe9d
judymand <57102527+judymand@users.noreply.github.com>
2021-04-29 12:14:20 +0300

    Add routers to doctor pages.

--- src/routes/profile_route.js
+++ src/routes/profile_route.js
@@ -41,6 +41,53 @@ profile
         res.render('doctor_profile',{data:req.user})
     })
     .post(user_controller.allowIfLoggedin,user_controller.grantAccess('updateOwn','doctor_profile'),user_controller.updateUser)
+
+//doctor settings
+profile
+    .route('/doctor_profile/doctor_settings/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readOwn','patient_profile'),user_controller.grantAccess('readOwn','doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('doctor_settings',{data:req.user})
+    })
+    .post(user_controller.allowIfLoggedin,user_controller.grantAccess('updateOwn','doctor_profile'),user_controller.updateUser)
+
+
+//previous and future appointments
+profile
+    .route('/doctor_profile/doctor_future_appointments/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('doctor_future_appointments',{data:req.user})
+    })
+
+profile
+    .route('/doctor_profile/doctor_previous_appointments/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('doctor_previous_appointments',{data:req.user})
+    })
+
+// doctor opinions    
+profile
+    .route('/doctor_profile/doctor_opinions/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('doctor_opinions',{data:req.user})
+    })
+
+
+// doctor Appointment summary   
+profile
+    .route('/doctor_profile/doctor_appointment_summary/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('doctor_appointment_summary',{data:req.user})
+    })
+
+// doctor Reviwe   
+profile
+    .route('/doctor_profile/Review/:id')
+    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'doctor_profile'), user_controller.getUser,(req,res)=>{
+        res.render('Review',{data:req.user})
+    })
+
+
+
 // TBD
 
 module.exports = profile
