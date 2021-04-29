f3c90343fd240931c3e31a1aa3295caa3b0c9d63
kartowka <antonfley@gmail.com>
2021-04-29 20:01:03 +0300

    refactor the role section

--- src/roles/roles.js
+++ src/roles/roles.js
@@ -1,35 +1,30 @@
 const AccessControl = require('accesscontrol')
 const ac = new AccessControl()
 
-const doctor_attri = ['doctor_settings', 'doctor_appointment_summary', 'doctor_future_appointments']
-const patient_attri = ['patient_settings', 'patient_appointment_summary', 'patient_future_appointments', 'patient_previous_appointments']
+const doctor_paths = ['doctor_settings', 'doctor_appointment_summary', 'doctor_future_appointments','doctor_Review']
+const patient_paths = ['patient_settings', 'patient_appointment_summary', 'patient_future_appointments', 'patient_previous_appointments']
+const admin_paths = ['doctor_settings', 'doctor_appointment_summary', 'doctor_future_appointments','doctor_Review','patient_settings', 'patient_appointment_summary', 'patient_future_appointments', 'patient_previous_appointments','admin_interface']
 
 exports.roles = (function () {
     ac.grant('patient')
         .readOwn('patient_profile')
-        .readOwn(patient_attri)
-        .updateOwn('patient_profile')
-        .readAny('doctor_profile')
-        .updateOwn(patient_attri)
-
-    ac.deny('patient')
-        .readAny(doctor_attri)
+        .readOwn(patient_paths)
+        .updateOwn(patient_paths)
+        .readAny('doctor_profile')       
     
     ac.grant('doctor')
-        .readOwn(doctor_attri)
-        .updateOwn('doctor_profile')
-        .readAny(['patient_profile', 'doctor_profile'])
-        .deleteAny('forumQuestions')
-
-    ac.deny('doctor')
-        .readAny(patient_attri)
+        .readAny('doctor_profile')
+        .readOwn(doctor_paths)
+        .updateOwn(doctor_paths)
+        .readOwn('patient_profile')
 
     ac.grant('admin')
-        .readAny(doctor_attri)
-        .readAny(patient_attri)
-        .readAny(['patient_profile', 'doctor_profile', 'admin_interface'])
-        .updateAny(['patient_profile', 'doctor_profile', 'admin_interface'])
-        .deleteAny(['patient_profile', 'doctor_profile', 'admin_interface'])
+        .extend('patient')
+        .extend('doctor')
+        .readAny(admin_paths)
+        .updateAny(admin_paths)
+        .deleteAny(admin_paths)
+     
     
     return ac
 })()
