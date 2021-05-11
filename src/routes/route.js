d25b5410d1858cdcbb0661fb5602af3d98867857
kartowka <antonfley@gmail.com>
2021-05-11 15:38:48 +0300

    imporved routing for adminpage and restiction page

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,119 +4,123 @@ const user_controller = require('../controllers/user_controller')
 const profile_router = require('./profile_route')
 
 //homepage
-router
-    .route('/')
-    .get((req, res) => {
-        res.render('index')
-    })
+router.route('/').get((req, res) => {
+  res.render('index')
+})
 //our team
-router
-    .route('/our_team')
-    .get((req, res) => {
-        res.render('our_team')
-    })
+router.route('/our_team').get((req, res) => {
+  res.render('our_team')
+})
 // restricted
-router
-    .route('/restricted')
-    .get((req, res) => {
-        res.render('restricted', { alert: req.app.locals.errors })
-        req.app.locals.errors = []
-    })
+// router
+//     .route('/restricted')
+//     .get((req, res) => {
+//         res.render('restricted', { alert: req.sharon})
+//         req.app.locals.errors = []
+//     })
 //login
 router
-    .route('/login')
-    .get((req, res) => {
-        res.render('login')
-    })
-    .post(user_controller.login, (req, res) => {
-        res.render('login', { alert: req.app.locals.errors })
-        req.app.locals.errors = []
-    })
+  .route('/login')
+  .get(user_controller.denyIfLoggedin, (req, res) => {
+    if (res.statusCode != 200) {
+      res.render('restricted', { alert: req.error })
+    } else
+    console.log(req.error) 
+    res.render('login',{alert:req.error})
+
+  })
+  .post(user_controller.login, (req, res) => {
+    res.render('login', { alert: req.error })
+  })
 //logout
-router
-    .route('/logout')
-    .get((req, res) => {
-        res.clearCookie('x-access-token')
-        res.redirect('/')
-    })
+router.route('/logout').get((req, res) => {
+  res.clearCookie('x-access-token')
+  res.redirect('/')
+})
 //signup
 router
-    .route('/register')
-    .get((req, res) => {
-        res.render('register')
-    })
-    .post(user_controller.signup, (req, res) => {
-        res.render('login', { alert: req.app.locals.errors })
-        req.app.locals.errors = []
-    })
+  .route('/register')
+  .get(user_controller.denyIfLoggedin, (req, res) => {
+    if (res.statusCode != 200) {
+      res.render('restricted', { alert: req.error })
+    } else res.render('register')
+  })
+  .post(user_controller.signup, (req, res) => {
+    res.render('register',{alert: req.error})
+  })
 
 //signup
 router
-    .route('/travel_insurance')
-    .get((req, res) => {
-        res.render('travel_insurance')
-    })
-    .post((req,res)=>{
-        res.status(200).send(req.body)
-        console.log(req.body)
-    })
+  .route('/travel_insurance')
+  .get((req, res) => {
+    res.render('travel_insurance')
+  })
+  .post((req, res) => {
+    res.status(200).send(req.body)
+    console.log(req.body)
+  })
 
 //forgot password
 router
-    .route('/forgot_password')
-    .get((req, res) => {
-        res.render('forgot_password')
-    })
-    .post(user_controller.forgotPassword)
+  .route('/forgot_password')
+  .get(user_controller.denyIfLoggedin, (req, res) => {
+    if (res.statusCode != 200) {
+      res.render('restricted', { alert: req.error })
+    } else res.render('forgot_password')
+  })
+  .post(user_controller.forgotPassword)
 
 //password reset
 router
-    .route('/reset_password/:accessToken')
-    .get(async (req, res) => {
-        const { accessToken } = req.params
-        res.render('reset_password')
-    })
-    .post(user_controller.passwordReset)
+  .route('/reset_password/:accessToken')
+  .get(user_controller.denyIfLoggedin, async (req, res) => {
+    if (res.statusCode != 200) {
+      res.render('restricted', { alert: req.error })
+    } else {
+      const { accessToken } = req.params
+      res.render('reset_password')
+    }
+  })
+  .post(user_controller.passwordReset)
 
-//confirmation code    
-router
-    .route('/confirm/:confirmationCode')
-    .get(user_controller.verifyUser)
+//confirmation code
+router.route('/confirm/:confirmationCode').get(user_controller.verifyUser)
 
 router.use(profile_router)
-
-//previous appointments
 
 
 //admin interface
 router
-    .route('/admin_interface/:id')
-    .get(user_controller.allowIfLoggedin, user_controller.grantAccess('readAny', 'admin_interface'), user_controller.getUsers, (req, res) => {
-        res.render('admin_interface', { users: req.params.users })
-    })
-//admin edit user
-//.put(user_controller.allowIfLoggedin, user_controller.grantAccess('updateAny', 'admin_interface'), user_controller.updateUser) 
+  .route('/admin_interface/:id')
+  .get(
+    user_controller.allowIfLoggedin,
+    user_controller.grantAccess('readAny', 'admin_interface'),
+    user_controller.getUsers,
+    (req, res) => {
+      res.render('admin_interface', { users: req.params.users,alert:req.error})
+    }
+  )
+  .post(user_controller.getUsers,async (req, res) => {
+      if (Object.keys(req.body)[0] === 'delete_ID')
+        await user_controller.deleteUser(req, res)
+      if (Object.keys(req.body)[0] === 'approve_ID')
+        await user_controller.verifyDoctor(req, res)
+    //res.render('admin_interface', { users: req.params.users, alert: req.error })
+  })
 
-//admin delete user
 router
-    .route('/admin_interface/delete/:id')
-    .post(user_controller.deleteUser)
-
-router
-    .route('/admin_interface/approve/:id')
-    .post(user_controller.verifyDoctor)
-
-router
-    .route('/search')
-    .get(user_controller.allowIfLoggedin, user_controller.getUsers, (req, res) => {
-        res.render('search', { users: req.params.users })
-    })
+  .route('/search')
+  .get(
+    user_controller.allowIfLoggedin,
+    user_controller.getUsers,
+    (req, res) => {
+      res.render('search', { users: req.params.users })
+    }
+  )
 router.use(function (req, res, next) {
-
-    // you can do what ever you want here
-    // for example rendering a page with '404 Not Found'
-    res.status(404)
-    res.render('404', { error: 'Not Found' })
-
+  // you can do what ever you want here
+  // for example rendering a page with '404 Not Found'
+  res.status(404)
+  res.render('404', { error: 'Not Found' })
 })
 module.exports = router
