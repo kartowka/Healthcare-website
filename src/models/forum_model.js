0ce2b21c29368befa6d9eb3c934653b0a2969ee5
Sharon Yaroshetsky <imso3k@gmail.com>
2021-05-11 19:47:21 +0300

    re-factored to fit our ERD

--- src/models/forum_model.js
+++ src/models/forum_model.js
@@ -1,17 +1,49 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const forumSchema = new Schema({
-    type: {
+const questionSchema = new Schema({
+    topic: {
         type: String,
         required: true,
-        enum: ['topic', 'comment'],
     },
     date: {
         type: Date,
         required: true,
     },
-    //patient and doctor information like name can be taken from their own Schema
+    question_body: {
+        type: String,
+        required: true,
+    },
+    asked_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+})
+
+const commentSchema = new Schema({
+    date: {
+        type: Date,
+        required: true,
+    },
+    comment_body: {
+        type: String,
+        required: true,
+    },
+    commented_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+})
+
+const forumSchema = new Schema({
+    question: {
+        type: questionSchema,
+        required: true,
+    },
+    comment: {
+        type: commentSchema,
+        required: true,
+    },
 })
 
 const Forum = mongoose.model('user', forumSchema)
