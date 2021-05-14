f31236efeaa0a5ed00755a2049847fa8d17d36f2
Sharon Yaroshetsky <imso3k@gmail.com>
2021-05-14 14:13:02 +0300

    added new parameters for routes to match ejs buttons & text fields

--- src/routes/forum_route.js
+++ src/routes/forum_route.js
@@ -12,13 +12,13 @@ router.get('/', async (req, res) => {
   res.render('forum_dir/forum', { forums: forum })
 })
 
-router.get('/new', (req, res) => {
-  res.render('forum_dir/new', { forum: new Forum() })
+router.get('/new_forum', (req, res) => {
+  res.render('forum_dir/new_forum', { forum: new Forum(), button: 'new' })
 })
 
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit_forum/:id', async (req, res) => {
   const forum = await Forum.findById(req.params.id)
-  res.render('forum_dir/edit', { forum: forum })
+  res.render('forum_dir/edit_forum', { forum: forum, button: 'edit' })
 })
 router.post(
   '/',
@@ -26,7 +26,7 @@ router.post(
     req.forum = new Forum()
     next()
   },
-  saveforumAndRedirect('new')
+  saveforumAndRedirect('new_forum')
 )
 
 router.put(
@@ -35,7 +35,7 @@ router.put(
     req.forum = await Forum.findById(req.params.id)
     next()
   },
-  saveforumAndRedirect('edit')
+  saveforumAndRedirect('edit_forum')
 )
 
 router.delete('/:id', async (req, res) => {
@@ -53,8 +53,8 @@ function saveforumAndRedirect(path) {
     forum._doctor_id = res.locals.loggedInUser._id
     forum.slug = slugify(forum.title, { lower: true, strict: true })
     try {
-      if (path == 'new') forum = await forum.save()
-      if (path == 'edit') {
+      if (path == 'new_forum') forum = await forum.save()
+      if (path == 'edit_forum') {
         await Forum.findByIdAndUpdate(forum._id, {
           $set: {
             title: forum.title,
@@ -63,7 +63,7 @@ function saveforumAndRedirect(path) {
           },
         })
       }
-      res.redirect(`/forum/${forum.slug}`)
+      res.redirect('/forum')
     } catch (e) {
       res.render(`forum_dir/${path}`, { forum: forum })
     }
@@ -78,8 +78,6 @@ function saveforumAndRedirect(path) {
 //!----------- QUESTIONS ------------ //
 router.get('/:slug', async (req, res) => {
   const subForum = await Forum.findOne({ slug: req.params.slug })
-  //subForum.question={topic:'1',question_body:'test',asked_by:subForum._id}
-  //await Forum.findByIdAndUpdate(subForum._id,{$push:{question:{topic:'1',question_body:'test211',asked_by:subForum._id}}})
   if (subForum == null) res.redirect('/forum')
   res.render('forum_dir/sub_forum_dir/sub_forum', {
     questions: subForum.question,
@@ -91,7 +89,9 @@ router.get('/:slug', async (req, res) => {
 router.get('/:slug/new_question', async (req, res) => {
   const question = await Forum.findOne({ slug: req.params.slug })
   res.render('forum_dir/sub_forum_dir/new_question', {
+    question: '',
     subForumSlug: question.slug,
+    button: 'create',
   })
 })
 router.post(
@@ -106,10 +106,11 @@ router.post(
 )
 router.get('/:slug/edit/:id', async (req, res) => {
   let forum = await Forum.findOne({ slug: req.params.slug })
-  const questions = forum.question.id(req.params.id)
+  const question = forum.question.id(req.params.id)
   res.render('forum_dir/sub_forum_dir/edit_question', {
-    forum: questions,
+    question: question,
     subForumSlug: forum.slug,
+    button: 'edit',
   })
 })
 router.put(
@@ -123,7 +124,6 @@ router.put(
   saveQuestionAndRedirect('edit_question')
 )
 router.delete('/:slug/:id', async (req, res) => {
-  let forum = await Forum.findOne({ slug: req.params.slug })
   await Forum.updateOne({}, { $pull: { question: { _id: req.params.id } } })
   res.redirect(`/forum/${req.params.slug}`)
 })
@@ -188,8 +188,10 @@ router.get('/:slug/:id', async (req, res) => {
 router.get('/:slug/:id/new_comment', async (req, res) => {
   const forum = await Forum.findOne({ slug: req.params.slug })
   res.render('forum_dir/sub_forum_dir/new_comment', {
+    comment: '',
     subForumSlug: forum.slug,
     question: req.params.id,
+    button: 'create',
   })
 })
 router.post(
@@ -206,11 +208,14 @@ router.post(
 
 router.get('/:slug/:questionID/edit/:commentID', async (req, res) => {
   let forum = await Forum.findOne({ slug: req.params.slug })
-  const question = forum.question.id(req.params.questionID)
+  let question = forum.question.id(req.params.questionID)
+  let comment = question.comment.id(req.params.commentID)
   res.render('forum_dir/sub_forum_dir/edit_comment', {
+    comment: comment.comment_body,
     question: req.params.questionID,
     subForumSlug: forum.slug,
     commentID: req.params.commentID,
+    button: 'edit',
   })
 })
 router.put(
@@ -232,7 +237,7 @@ router.delete('/:slug/:questionID/:commentID', async (req, res) => {
     forum._id,
     {
       $pull: {
-        'question.$[i].comment': { '_id': req.params.commentID },
+        'question.$[i].comment': { '_id': req.params.commentID }, //[i] == questionID
       },
     },
     {
@@ -253,23 +258,26 @@ function saveCommentAndRedirect(path) {
     const question = forum.question.id(req.params.id)
     let user = await User.findById(res.locals.loggedInUser._id)
     let full_name = user.first_name + ' ' + user.last_name
+    if (user.role == 'doctor') {
+      full_name = 'Dr.' + ' ' + user.last_name
+    }
     try {
       if (path == 'new_comment') {
         await Forum.findByIdAndUpdate(
           forum._id,
           {
             $push: {
-              'question.$[question].comment': {
+              'question.$[i].comment': { //[i] == question._id
                 comment_body: comment.comment_body,
                 commented_by: full_name,
-                commeted_by_id: user._id,
+                commented_by_id: user._id,
               },
             },
           },
           {
             arrayFilters: [
               {
-                'question._id': question._id,
+                'i._id': question._id,
               },
             ],
           }
@@ -280,10 +288,10 @@ function saveCommentAndRedirect(path) {
           forum._id,
           {
             $set: {
-              'question.$[i].comment.$[j]': {
+              'question.$[i].comment.$[j]': { //[i] == question.ID, [j] == comment.ID
                 comment_body: comment.comment_body,
                 commented_by: full_name,
-                commeted_by_id: user._id,
+                commented_by_id: user._id,
               },
             },
           },
