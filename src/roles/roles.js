fe705fa176bcfb57a99d435cca3cc3eb571a0875
kartowka <antonfley@gmail.com>
2021-05-14 16:43:11 +0300

    added new paths for forum

--- src/roles/roles.js
+++ src/roles/roles.js
@@ -4,11 +4,17 @@ const ac = new AccessControl()
 const doctor_paths = ['doctor_settings', 'doctor_appointment_summary', 'doctor_future_appointments','doctor_Review']
 const patient_paths = ['patient_settings', 'patient_appointment_summary', 'patient_future_appointments', 'patient_previous_appointments']
 const admin_paths = ['doctor_settings', 'doctor_appointment_summary', 'doctor_future_appointments','doctor_Review','patient_settings', 'patient_appointment_summary', 'patient_future_appointments', 'patient_previous_appointments','admin_interface']
+const forum_paths = ['forum','new_forum','edit_forum','_form_fiels']
+const question_paths = ['sub_forum','new_question','edit_question','question_form_fields']
+const comment_paths = ['conversation','new_comment','edit_comment','comment_form_fields']
 
 exports.roles = (function () {
     ac.grant('patient')
         .readOwn('patient_profile')
         .readOwn(patient_paths)
+        .readOwn(forum_paths)
+        .readOwn(question_paths)
+        .readOwn(comment_paths)
         .updateOwn(patient_paths)
         .readAny('doctor_profile')       
     
