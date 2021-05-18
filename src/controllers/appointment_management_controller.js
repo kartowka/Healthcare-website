3b77d0913f2f1fc0cc8b427be8dc0fa268a4c7d8
יהודית מנדלבוים <judy@yhwdyts-MacBook-Pro.local>
2021-05-18 15:35:01 +0300

    Added a function that create a new appointment.

--- src/controllers/appointment_management_controller.js
+++ src/controllers/appointment_management_controller.js
@@ -1,24 +1,34 @@
-//const User = require('../models/user_model')
 const DoctorDetails = require('../models/doctor_model')
 const Appointment = require('../models/appointment_model')
 
 exports.make_an_Appointment = async (req, res, next) => {
 	try {
-		let update = req.body
-		let userId = req.params.id
-		// let user = await User.findById(userId)
-		let doctor = await DoctorDetails.findOne({ _doctor_id: userId })
+		let appointment_details = req.body
+		let doctor_id = await DoctorDetails.findOne({ _doctor_id: req.params.id })
+		let full_date = appointment_details.date.split(' ')
+		
+		if(doctor_id == res.locals.loggedInUser._id){
+			throw 'A doctor cannot make an appointment for himself!'
+		}
 
+		
 		const newAppointment = new Appointment({
-			start_time: update.start_time,
-			date: update.date,
-			appointment_subject: update.appointment_subject,
-			doctor: doctor,
-			// patient: user,
-			type: update.type,
+			start_time: full_date[1],
+			date: full_date[0],
+			appointment_subject: appointment_details.appointment_subject,
+			doctor: doctor_id,
+			patient: res.locals.loggedInUser._id,
+			type: appointment_details.type,
 		})
+		await newAppointment.save()
+		next()
+
+	
+
+		
 	} catch (error) {
 		let statusCode = '401'
+		console.log(error)
 		res.redirect(`/restricted/${error}/${statusCode}`)
 	}
 }
