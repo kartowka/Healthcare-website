d4617a9cb9b8294ba65f90eeb22a244edbb42bdc
יהודית מנדלבוים <judy@yhwdyts-MacBook-Pro.local>
2021-05-18 17:26:57 +0300

    Added a js page for calendar.

---
+++ src/js/appointment_management.js
@@ -0,0 +1,19 @@
+let working_days = document.getElementById("working_days").textContent;
+let enabel_day = working_days.split(',')
+let dic = {
+   'Sunday': 0, 
+   'Monday': 1,
+   'Tuesday': 2, 
+   'Wednesday': 3,
+   'Thursday': 4,
+    'Friday': 5,
+    'Saturday': 6
+}
+let day_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+day_of_week = day_of_week.filter(n => !enabel_day.includes(n))
+day_of_week = day_of_week.map(n => dic[n])
+$('.form_datetime').datetimepicker({
+   format: 'dd/mm/yyyy hh:ii',
+   daysOfWeekDisabled: day_of_week,
+ 
+})
