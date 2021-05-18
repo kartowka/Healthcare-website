30d0b39c014219ae00a5ee0f7688c88472edaed5
kartowka <antonfley@gmail.com>
2021-05-18 13:16:11 +0300

    error handle

--- src/controllers/appointment_management_controller.js
+++ src/controllers/appointment_management_controller.js
@@ -2,28 +2,23 @@
 const DoctorDetails = require('../models/doctor_model')
 const Appointment = require('../models/appointment_model')
 
-
 exports.make_an_Appointment = async (req, res, next) => {
-    
-    try {
-        let update = req.body
-        let userId = req.params.id
-        // let user = await User.findById(userId)
-        let doctor = await DoctorDetails.findOne({ _doctor_id: userId })
-        
-        const newAppointment = new Appointment({
-            start_time: update.start_time,
-            date: update.date, 
-            appointment_subject: update.appointment_subject,
-            doctor: doctor,
-            // patient: user,
-            type: update.type,
-        }) 
+	try {
+		let update = req.body
+		let userId = req.params.id
+		// let user = await User.findById(userId)
+		let doctor = await DoctorDetails.findOne({ _doctor_id: userId })
 
-    
-    } catch (error) {
-        req.error = { Message: error, statusCode: '200' }
-        res.status(200)
-        res.redirect(req.get('referer'))
-    }
+		const newAppointment = new Appointment({
+			start_time: update.start_time,
+			date: update.date,
+			appointment_subject: update.appointment_subject,
+			doctor: doctor,
+			// patient: user,
+			type: update.type,
+		})
+	} catch (error) {
+		let statusCode = '401'
+		res.redirect(`/restricted/${error}/${statusCode}`)
+	}
 }
