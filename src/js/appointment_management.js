ad1de1266f8d02835aecfd01f2c5ab1e0e4dc3f4
יהודית מנדלבוים <judy@yhwdyts-MacBook-Pro.local>
2021-05-18 18:08:50 +0300

    limit the hours of the appointment According to the doctor's working hours.

--- src/js/appointment_management.js
+++ src/js/appointment_management.js
@@ -1,4 +1,6 @@
 let enabel_day = document.getElementById('working_days').textContent.split(',')
+let start_time = document.getElementById("start_time").textContent.split(':')
+let end_time = document.getElementById("end_time").textContent.split(':')
 let dic = {
    'Sunday': 0, 
    'Monday': 1,
@@ -9,10 +11,15 @@ let dic = {
     'Saturday': 6
 }
 let day_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+let hoursEnable = hours.filter(value => start_time[0] <= value  && value <= end_time[0])
+let disablehour = hours.filter(n => !hoursEnable.includes(n))
 day_of_week = day_of_week.filter(n => !enabel_day.includes(n))
 day_of_week = day_of_week.map(n => dic[n])
 $('.form_datetime').datetimepicker({
    format: 'dd/mm/yyyy hh:ii',
    daysOfWeekDisabled: day_of_week,
+   hoursDisabled: disablehour,
+
  
 })
