297f72769c862fab30e26c1a84b90d9ccf57b944
kartowka <antonfley@gmail.com>
2021-05-19 15:47:10 +0300

    rebuilt search function

--- src/routes/search_route.js
+++ src/routes/search_route.js
@@ -4,81 +4,59 @@ const user_controller = require('../controllers/user_controller')
 const User = require('../models/user_model')
 const DoctorDetails = require('../models/doctor_model')
 
-router
-	.route('/')
-	.get(
-		user_controller.allowIfLoggedin,
-		user_controller.getUsers,
-		async (req, res) => {
-			try {
-				const { sort_by, term, sort_order } = req.query
-				const users = await User.find({
-					last_name: { $regex: term, $options: 'i' },
-				})
-					.where('role')
-					.equals('doctor')
-				var doctors = []
-				for (let user of users) {
-					let doctor_details = await DoctorDetails.findOne({
-						_doctor_id: user._id,
-					})
+router.route('/').get(user_controller.allowIfLoggedin, async (req, res) => {
+	try {
+		var doctors = []
+		const { term } = req.query
+		await DoctorDetails.find({
+			$text: { $search: term, $caseSensitive: false },
+		})
+			.then(async (users) => {
+				for (let user in users) {
+					let getUserFromDB = await User.findById(users[user]._doctor_id)
 					let doctor = {
-						first_name: user.first_name,
-						last_name: user.last_name,
-						city: doctor_details.clinic_address.city,
-						specialization: doctor_details.specialization,
-						working_days: doctor_details.working_days,
-						spoken_languages: doctor_details.spoken_languages,
-						doctor_related_clinics: doctor_details.doctor_related_clinics,
-						_id: doctor_details._doctor_id,
+						first_name: getUserFromDB.first_name,
+						last_name: getUserFromDB.last_name,
+						city: users[user].clinic_address.city,
+						specialization: users[user].specialization,
+						working_days: users[user].working_days,
+						spoken_languages: users[user].spoken_languages,
+						doctor_related_clinics: users[user].doctor_related_clinics,
+						_id: users[user]._doctor_id,
 					}
 					doctors.push(doctor)
 				}
-				if (sort_by != '') sortBy(sort_by, doctors, sort_order)
-				res.render('search', { doctors: doctors })
-			} catch (e) {
+			})
+			.catch((e) => {
 				//console.log(e)
-				res.render('search', { doctors: '' })
-			}
-		}
-	)
-
-function sortBy(sort_by, doctors, sort_order) {
-	if (sort_by == 'working_days') {
-		let days = {
-			Sunday: '0',
-			Monday: '1',
-			Tuesday: '2',
-			Wednsday: '3',
-			Thursday: '4',
-			Friday: '5',
-			Saturday: '6',
-		}
-		doctors.sort((first, second) => {
-			first = days[first[sort_by][0]]
-			second = days[second[sort_by][0]]
-			return first < second ? 0 : 1
-		})
-	} else if (
-		sort_by == 'spoken_languages' ||
-		sort_by == 'specialization' ||
-		sort_by == 'doctor_related_clinics'
-	) {
-		doctors.sort((first, second) => {
-			first = first[sort_by][0].toUpperCase()
-			second = second[sort_by][0].toUpperCase()
-			if (first < second) return -1
-			if (first > second) return 1
-			return 0
-		})
-	} else
-		doctors.sort((first, second) => {
-			var first_parm = first[sort_by].toUpperCase()
-			var second_parm = second[sort_by].toUpperCase()
-			if (first_parm < second_parm) return -1
-			if (first_parm > second_parm) return 1
-			return 0
-		})
-	if (sort_order == 'descending') doctors.reverse()
-}
+			})
+		await User.find({ $text: { $search: term, $caseSensitive: false } })
+			.where('role')
+			.equals('doctor')
+			.then(async (users) => {
+				for (let user in users) {
+					let getUserFromDB = await DoctorDetails.findOne({
+						_doctor_id: users[user]._id,
+					})
+					let doctor = {
+						first_name: users[user].first_name,
+						last_name: users[user].last_name,
+						city: getUserFromDB.clinic_address.city,
+						specialization: getUserFromDB.specialization,
+						working_days: getUserFromDB.working_days,
+						spoken_languages: getUserFromDB.spoken_languages,
+						doctor_related_clinics: getUserFromDB.doctor_related_clinics,
+						_id: getUserFromDB._doctor_id,
+					}
+					doctors.push(doctor)
+				}
+			})
+			.catch((e) => {
+				//console.log(e)
+			})
+		res.render('search', { doctors: doctors })
+	} catch (e) {
+		res.render('search', { doctors: '' })
+	}
+})
 module.exports = router
