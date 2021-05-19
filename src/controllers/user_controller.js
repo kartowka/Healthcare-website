4688e2f4b28443b2dd25599cb3ff05b21a5c072e
kartowka <antonfley@gmail.com>
2021-05-19 15:43:08 +0300

    updateUser now split spoken_lang to words in array

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -187,7 +187,6 @@ exports.updateUser = async (req, res, next) => {
 		let update = req.body
 		let userId = req.params.id
 		let user = await User.findById(userId)
-
 		if (update.first_name != undefined) {
 			if (update.first_name && update.last_name) {
 				let userUpdate = await User.findByIdAndUpdate(userId, update)
@@ -219,7 +218,7 @@ exports.updateUser = async (req, res, next) => {
 						{
 							$set: {
 								'clinic_address.city': update.city,
-								spoken_languages: update.spoken_languages,
+								spoken_languages: update.spoken_languages.split(','),
 								'clinic_address.street': update.street,
 								clinic_phone_number: update.clinic_phone_number,
 							},
