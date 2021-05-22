4031ee94536099d55be7b8fea3483a9e35cd2789
יהודית מנדלבוים <judy@yhwdyts-MacBook-Pro.local>
2021-05-22 22:56:38 +0300

    Added a getAppoientment function to a patient profile routing.

--- src/routes/profile_route.js
+++ src/routes/profile_route.js
@@ -12,8 +12,14 @@ profile
 		user_controller.allowIfLoggedin,
 		user_controller.grantAccess('readOwn', 'patient_profile'),
 		user_controller.getUser,
+		appointment_management_controller.getAppointments,
 		(req, res) => {
-			res.render('patient_profile', { data: req.user })
+			res.render('patient_profile', {
+				data: req.user,
+				appointment_details: req.appointment_details,
+				doctor_details: req.doctor_details,
+				user_doctor: req.user_doctor,
+			})
 		}
 	)
 
